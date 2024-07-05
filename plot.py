b462a40b5783ffc33c0f672c4b7a895a50d123a2
Kazuha Ayato <66939153+kazuhaAyato@users.noreply.github.com>
2024-07-05 09:30:58 +0800

    Plot sin(x) and cos(x)

---
+++ plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+#plot graph of sin(x)
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+#plot graph of cos(x)
+x = np.linspace(0, 10, 100)
+y = np.cos(x)
+plt.plot(x, y)
+#plot graph of tan(x) idk
+plt.show()
